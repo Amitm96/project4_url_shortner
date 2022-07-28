6d960351fa1eb2034a926abbaa0f87eb10bc3eec
Amitmaz96 <amitm9088@gmail.com>
2022-07-28 19:20:52 +0530

    adding home page

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const route = require('./routes/route');
 const  mongoose = require('mongoose');
+const ejs = require('ejs')
+const path = require('path')
 // instatiate the express app  
 const app = express()
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
+
+app.engine('html' , ejs.renderFile)
+app.set('view engine' , 'html')
+app.set('views' , path.join(__dirname , '../views'))
    
 mongoose.connect("mongodb+srv://Amitmaz96:FuYghhKoFzigilxK@cluster1.mdpsbcj.mongodb.net/urlDb")
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const urlController = require("../Controllers/urlController");
 
+router.get("/" , async function(req , res){
+    res.status(200).render('index.html')
+})
 router.post("/url/shorten",urlController.shortenUrl)
 router.get("/:urlCode",urlController.getUrlCode)
 
